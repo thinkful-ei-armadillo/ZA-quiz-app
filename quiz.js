0aa7aede2695e7529543e77368e281b40aee6462
Andrew Bituin <fandrewbituin@gmail.com>
2019-01-22 12:49:51 -0800

    added functionality up to feedback state

--- quiz.js
+++ quiz.js
@@ -1,11 +1,23 @@
+
 'use strict';
+
+
+const QUESTIONS = [
+    {
+        question: 'What year did Steven Gerrard joined Liverpool FC?',
+        options: [1990, 1998, 2002, 2004],
+        correctAns: 1998
+
+    }
+];
 
 const STORE = {
   currentState: 'start',
   currentQuestion: '',
-  userAnswer: null,
+  userAnswer: '',
   currentScore: 0,
-  questionNumber: null
+  questionNumber: 0,
+  correctAnswer: ''
 
 };
 
@@ -16,36 +28,37 @@ function generateStartHtmlString(){
   return `<section class="start-page">
     <p>How much of a Liverpool FC supporter are you?</p>
     <p></p>
-    <button class="start-button">Start Quiz</button>
+    <button class="js-start-button">Start Quiz</button>
 </section>`;
 }
 
-function generateHtmlQuestionsString(){
-  return `<div class="question-page">
+function generateHtmlQuestionsString(state, question){
+  const currentQuestion = question[state.questionNumber - 1].question;
+  const options = question[state.questionNumber - 1].options;
+    return `<div class="question-page">
     <div class="quiz-info-section">
-        <p>Question <span class="current-question-number">1</span> of <span class="total-question-number">10</span></p>
-        <p><span class="correct-score-count">0</span>/<span class="total-question-number">10</span></p>
+        <p>Question <span class="current-question-number">${state.questionNumber}</span> of <span class="total-question-number">${QUESTIONS.length}</span></p>
+        <p><span class="correct-score-count">${state.currentScore}</span>/<span class="total-question-number">${QUESTIONS.length}</span></p>
     </div>
     <div class="current-question-section">
         <form>
-            <p class="current-question">What year did Steven Gerrard joined Liverpool FC?</p>
+            <p class="current-question">${currentQuestion}</p>
             <ul id="options">
-                <li><input type="radio" id="Option-one" name="option"><label for="Option-one"> 1999</label></li>
-                <li><input type="radio" id="Option-two" name="option"><label for="Option-two"> 2002</label></li>
-                <li><input type="radio" id="Option-three" name="option"><label for="Option-three"> 1998</label></li>
-                <li><input type="radio" id="Option-four" name="option"><label for="Option-four"> 2005</label></li>
+                <li><input type="radio" id="Option-one" name="option" value="${options[0]}"/>${options[0]}</li>
+                <li><input type="radio" id="Option-two" name="option" value="${options[1]}"/>${options[1]}</li>
+                <li><input type="radio" id="Option-three" name="option" value="${options[2]}"/>${options[2]}</li>
+                <li><input type="radio" id="Option-four" name="option" value="${options[3]}"/>${options[3]}</li>
             </ul>
-            <input type="button" value="Submit Answer" class="submit-answer-button">
+            <input type="button" value="Submit Answer" class="js-submit-answer-button">
         </form>
     </div>
 </div>`;
 }
 
-function generateHtmlFeedbackString(){
-  return `<div class="feedback-page">
+function generateHtmlFeedbackString(state){
+    return `<div class="feedback-page">
                 <div>
-                    <p class="feedback-message">Your answer was <span class="question-result"></span></p>
-                    <p class="correct-answer">The correct answer is <span class="correct-answer-result"></span></p>
+                    <p class="feedback-message">Your answer was <span class="question-result">${state.correctAnswer === state.userAnswer ? 'correct!' : 'incorrect! <p class="correct-answer">The correct answer is <span class="correct-answer-result"></span></p>'}</span></p>
                 </div>
                 <input type="button" value="Next Question" class="next-question-button">
             </div>`;
@@ -61,17 +74,42 @@ function generateHtmlResultString(){
 
 // Render HTML string to DOM
 function renderHtml(){
-  const startPage = generateStartHtmlString(STORE, QUESTIONS);
+    if (STORE.currentState === 'start'){
+        console.log('Start working');
+        $('.js-main').html(generateStartHtmlString());
+    }
+    else if (STORE.currentState === 'quiz'){
+        console.log('Quiz working');
+
+        $('.js-main').html(generateHtmlQuestionsString(STORE, QUESTIONS));
+    }
+    else if (STORE.currentState === 'feedback'){
+        console.log('Feedback working');
+        $('.js-main').html(generateHtmlFeedbackString(STORE));
+    }
 }
 
 // Handle start-quiz submit
 function handleStartQuiz(){
-
+    $('.js-main').on('click', '.js-start-button', function(event){
+        STORE.currentState = 'quiz';
+        STORE.currentQuestion = QUESTIONS[STORE.questionNumber].question;
+        // STORE.currentScore =
+        STORE.correctAnswer = QUESTIONS[STORE.questionNumber].correctAns;
+        STORE.questionNumber++;
+        renderHtml();
+    });
 }
 
 // Handle submit answer 
-function handleAnswerSubmit(){
-
+function handleAnswerSubmit(){    
+    $('.js-main').on('click', '.js-submit-answer-button', function(event){
+        const userAnswer = $(event.currentTarget).closest('form').find('input[name="option"]:checked').val();
+        console.log(userAnswer);
+        STORE.userAnswer = userAnswer;
+        STORE.currentState = 'feedback';
+        renderHtml();
+    });
 }
 
 // Handle next question 
@@ -85,7 +123,9 @@ function handleReset(){
 }
 
 function main(){
-
+    renderHtml();
+    handleStartQuiz();
+    handleAnswerSubmit();
 }
 
 $(main);
--- quizdata.js
+++ quizdata.js
@@ -1,19 +1,19 @@
 'use strict';
 
-const QUESTIONS = [
-    {
-        questionOne: 'Question?'
-        options: [one, two, three, four]
-        correctAns: this.options[1]
+// const QUESTIONS = [
+//     {
+//         questionOne: 'Question?'
+//         options: [one, two, three, four]
+//         correctAns: this.options[1]
 
-    },     {
-        questionOne: 'Question?'
-        options: [one, two, three, four]
-        correctAns: this.options[1]
+//     },     {
+//         questionOne: 'Question?'
+//         options: [one, two, three, four]
+//         correctAns: this.options[1]
           
-    },    {
-        questionOne: 'Question?'
-        options: [one, two, three, four]
-        correctAns: this.options[1]      
-    }
-];
+//     },    {
+//         questionOne: 'Question?'
+//         options: [one, two, three, four]
+//         correctAns: this.options[1]      
+//     }
+// ];
