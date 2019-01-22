c0289595d7af48c7bb0aa523765b7869326efbe9
Zoljargal Fallows <zol.fallows@hotmail.com>
2019-01-22 22:09:23 +0000

    added submit answer and reset functionalities

--- quiz.js
+++ quiz.js
@@ -3,12 +3,31 @@
 
 
 const QUESTIONS = [
-    {
-        question: 'What year did Steven Gerrard joined Liverpool FC?',
-        options: [1990, 1998, 2002, 2004],
-        correctAns: 1998
-
-    }
+  {
+    question: 'What year did Steven Gerrard joined Liverpool FC?',
+    options: ['1990', '1998', '2002', '2004'],
+    correctAns: '1998'
+  },
+  {
+    question: 'What country Mo Salah is from?',
+    options: ['Germany', 'UK', 'USA', 'Egypt'],
+    correctAns: 'Egypt'
+  },
+  {
+    question: 'What country Mo Salah is from?',
+    options: ['Germany', 'UK', 'USA', 'Egypt'],
+    correctAns: 'Egypt'
+  },
+  {
+    question: 'What country Mo Salah is from?',
+    options: ['Germany', 'UK', 'USA', 'Egypt'],
+    correctAns: 'Egypt'
+  },
+  {
+    question: 'What country Mo Salah is from?',
+    options: ['Germany', 'UK', 'USA', 'Egypt'],
+    correctAns: 'Egypt'
+  }
 ];
 
 const STORE = {
@@ -35,7 +54,7 @@ function generateStartHtmlString(){
 function generateHtmlQuestionsString(state, question){
   const currentQuestion = question[state.questionNumber - 1].question;
   const options = question[state.questionNumber - 1].options;
-    return `<div class="question-page">
+  return `<div class="question-page">
     <div class="quiz-info-section">
         <p>Question <span class="current-question-number">${state.questionNumber}</span> of <span class="total-question-number">${QUESTIONS.length}</span></p>
         <p><span class="correct-score-count">${state.currentScore}</span>/<span class="total-question-number">${QUESTIONS.length}</span></p>
@@ -43,89 +62,123 @@ function generateHtmlQuestionsString(state, question){
     <div class="current-question-section">
         <form>
             <p class="current-question">${currentQuestion}</p>
-            <ul id="options">
-                <li><input type="radio" id="Option-one" name="option" value="${options[0]}"/>${options[0]}</li>
-                <li><input type="radio" id="Option-two" name="option" value="${options[1]}"/>${options[1]}</li>
-                <li><input type="radio" id="Option-three" name="option" value="${options[2]}"/>${options[2]}</li>
-                <li><input type="radio" id="Option-four" name="option" value="${options[3]}"/>${options[3]}</li>
-            </ul>
-            <input type="button" value="Submit Answer" class="js-submit-answer-button">
+            <div id="options">
+                <label><input type="radio" id="Option-one" name="option" value="${options[0]}" required> ${options[0]}</label><br>
+                <label><input type="radio" id="Option-two" name="option" value="${options[1]}" required> ${options[1]}</label><br>
+                <label><input type="radio" id="Option-three" name="option" value="${options[2]}" required> ${options[2]}</label><br>
+                <label><input type="radio" id="Option-four" name="option" value="${options[3]}" required> ${options[3]}</label><br>
+                <input type="submit" value="Submit Answer" class="js-submit-answer-button">
+            </div>
         </form>
     </div>
 </div>`;
 }
 
 function generateHtmlFeedbackString(state){
-    return `<div class="feedback-page">
+  const isCorrect = state.correctAnswer === state.userAnswer;
+  return `<div class="feedback-page">
                 <div>
-                    <p class="feedback-message">Your answer was <span class="question-result">${state.correctAnswer === state.userAnswer ? 'correct!' : 'incorrect! <p class="correct-answer">The correct answer is <span class="correct-answer-result"></span></p>'}</span></p>
+                    <p class="feedback-message">Your answer was <span class="question-result">${isCorrect ? 'correct!' : 'incorrect!'}</span></p>
+                    ${isCorrect ? 'Awesome, off to next question....' : `<p class="correct-answer">The correct answer is ${state.correctAnswer}</p>`}
                 </div>
-                <input type="button" value="Next Question" class="next-question-button">
+                <input type="button" value="Next Question" class="js-next-question-button">
             </div>`;
 }
 
-function generateHtmlResultString(){
+function generateHtmlResultString(state){
   return `<div class="final-result">
-            <p class="result-score-message">Your score was <span class="result-score"></span></p>
-            <p class="message-based-on-result"></p>
-            <input type="button" value="Reset" class="reset-quiz-button">
+            <p class="result-score-message">Your score was <span class="result-score">${STORE.currentScore}</span></p>
+            <p class="message-based-on-result">Thanks for playing!</p>
+            <input type="button" value="Reset" class="js-reset-quiz-button">
         </div>`;
 }
 
 // Render HTML string to DOM
 function renderHtml(){
-    if (STORE.currentState === 'start'){
-        console.log('Start working');
-        $('.js-main').html(generateStartHtmlString());
-    }
-    else if (STORE.currentState === 'quiz'){
-        console.log('Quiz working');
+  if (STORE.currentState === 'start'){
+    console.log('Start working');
+    $('.js-main').html(generateStartHtmlString());
+  }
+  else if (STORE.currentState === 'quiz'){
+    console.log('Quiz working');
 
-        $('.js-main').html(generateHtmlQuestionsString(STORE, QUESTIONS));
-    }
-    else if (STORE.currentState === 'feedback'){
-        console.log('Feedback working');
-        $('.js-main').html(generateHtmlFeedbackString(STORE));
-    }
+    $('.js-main').html(generateHtmlQuestionsString(STORE, QUESTIONS));
+  }
+  else if (STORE.currentState === 'feedback'){
+    console.log('Feedback working');
+    $('.js-main').html(generateHtmlFeedbackString(STORE));
+  } else if(STORE.currentState === 'result') {
+    $('.js-main').html(generateHtmlResultString(STORE));
+  }
 }
 
 // Handle start-quiz submit
 function handleStartQuiz(){
-    $('.js-main').on('click', '.js-start-button', function(event){
-        STORE.currentState = 'quiz';
-        STORE.currentQuestion = QUESTIONS[STORE.questionNumber].question;
-        // STORE.currentScore =
-        STORE.correctAnswer = QUESTIONS[STORE.questionNumber].correctAns;
-        STORE.questionNumber++;
-        renderHtml();
-    });
+  $('.js-main').on('click', '.js-start-button', function(event){
+    STORE.currentState = 'quiz';
+    STORE.currentQuestion = QUESTIONS[STORE.questionNumber].question;
+    // STORE.currentScore =
+    STORE.correctAnswer = QUESTIONS[STORE.questionNumber].correctAns;
+    STORE.questionNumber++;
+    renderHtml();
+  });
+}
+
+function updateUserScore(){
+  if(STORE.correctAnswer === STORE.userAnswer){
+    STORE.currentScore++;
+  }
 }
 
 // Handle submit answer 
-function handleAnswerSubmit(){    
-    $('.js-main').on('click', '.js-submit-answer-button', function(event){
-        const userAnswer = $(event.currentTarget).closest('form').find('input[name="option"]:checked').val();
-        console.log(userAnswer);
-        STORE.userAnswer = userAnswer;
-        STORE.currentState = 'feedback';
-        renderHtml();
-    });
+function handleAnswerSubmit(){   
+    // debugger; 
+  $('.js-main').on('click', '.js-submit-answer-button', function(event){
+    // event.preventDefault();
+    const userAnswer = $(this).closest('form').find('input[name="option"]:checked').val();
+    console.log(userAnswer);
+    STORE.userAnswer = userAnswer;
+    STORE.currentState = 'feedback';
+    updateUserScore();
+    renderHtml();
+  });
 }
 
 // Handle next question 
 function handleNextQuestion(){
-
+  $('.js-main').on('click', '.js-next-question-button', function(event){
+    if (STORE.questionNumber < QUESTIONS.length){
+      STORE.currentQuestion = QUESTIONS[STORE.questionNumber].question;
+      STORE.correctAnswer = QUESTIONS[STORE.questionNumber].correctAns;
+      STORE.currentState = 'quiz';
+      STORE.questionNumber++;
+      renderHtml();
+    } else {
+      STORE.currentState = 'result';
+      renderHtml();
+    }
+  });
 }
 
 // handle reset quiz-app
 function handleReset(){
-
+  $('.js-main').on('click', '.js-reset-quiz-button', function(event){
+    STORE.currentState = 'start';
+    STORE.currentQuestion = '';
+    STORE.userAnswer = '';
+    STORE.currentScore = 0;
+    STORE.questionNumber = 0;
+    STORE.correctAnswer = '';
+    renderHtml();
+  });
 }
 
 function main(){
-    renderHtml();
-    handleStartQuiz();
-    handleAnswerSubmit();
+  renderHtml();
+  handleStartQuiz();
+  handleAnswerSubmit();
+  handleNextQuestion();
+  handleReset();
 }
 
 $(main);
